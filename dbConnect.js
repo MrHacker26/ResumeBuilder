03234c0ebcb3b5245042fee186db187ba1a51f70
Tarun <tjocz26@gmail.com>
2023-02-02 18:53:40 +0530

    update

--- dbConnect.js
+++ dbConnect.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
+mongoose.set('strictQuery', true);
+
 mongoose.connect(process.env.DB_URL , {useUnifiedTopology:true , useNewUrlParser:true})
 
 const connection = mongoose.connection
+
 
 connection.on('connected' , ()=>{
     console.log('Mongo DB Connection Successful')
